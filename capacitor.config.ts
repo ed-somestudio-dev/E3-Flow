f1693f34ebf701696100f87b9c5630bd47f83f3d
ed-somestudio-dev <ed-somestudio@live.com>
2026-06-22 17:44:43 -0300

    chore: rename app to FluxoPro

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.fluxopro.app',
-  appName: 'FluxoPro2',
+  appName: 'FluxoPro',
   webDir: 'dist'
 };
 
